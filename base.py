3dabb7baa693080c3b76ef4d06e3de62251bdc7e
betebetoven <67440333+betebetoven@users.noreply.github.com>
2022-10-03 00:57:12 -0600

    'yafuncionamostrarlostableros'

--- base.py
+++ base.py
@@ -1,4 +1,6 @@
+
 from tkinter import *
+
 from PIL import ImageTk,Image
 from django.forms import PasswordInput
 
@@ -6,9 +8,24 @@ from django.forms import PasswordInput
 
 root = Tk()
 root.title('Fase 2')
+def ver():
+    global compu
+    global disp_jug
+    global uwu
 
-top = Toplevel()
-top.title('LOGIN')
+    top = Toplevel()
+    f = Label(top,text="TABLERO COMPUTADORA").pack(side = LEFT, fill = Y)
+    compu = ImageTk.PhotoImage(Image.open('compu.png').resize((400,400)))
+    my_label = Label(top, image=compu).pack(side = LEFT, fill = Y)
+
+    f = Label(top,text="TABLERO disparos").pack(side = LEFT, fill = Y)
+    disp_jug = ImageTk.PhotoImage(Image.open('disp_jug.png').resize((400,400)))
+    my_label2 = Label(top, image=disp_jug).pack(side = LEFT, fill = Y)
+    
+    
+   
+
+btn = Button(root,text="ver foto",command=ver).pack()
 
 
 
