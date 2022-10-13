bf5bcb30fdae97fe25bbddcce79ab57d2d1a0d2b
betebetoven <67440333+betebetoven@users.noreply.github.com>
2022-10-13 15:06:37 -0600

    'terminandografo'

---
+++ LL.py
@@ -0,0 +1,8 @@
+from nodoLL import nodito
+class listaenlazada:
+    head = None
+    tamaño = 0
+    def __init__(self):
+        self.head = None
+        self.tamaño = 0
+    def agrega()
---
+++ nodoLL.py
@@ -0,0 +1,19 @@
+from tkinter.messagebox import NO
+
+
+class nodito:
+    value = None
+    Next = None
+    Der = None
+    def __init__(self, value) :
+        self.value = value
+        self.Next = None
+        self.Der = None
+    def __str__(self) :
+        jesus = ""
+        jesus+= f'{str(id(self))} [label=\"{str(self.value)}\",fillcolor =\"pink\"]'
+    def agragaderecha(self,nuevoval):
+        aux = self
+        while aux.Der != None:
+            aux = aux.Der
+        aux.Der = nodito(nuevoval)
