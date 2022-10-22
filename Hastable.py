88b877121c6d8f6cab0a4530341be99fe2d8f7e3
betebetoven <albertoha2000@gmail.com>
2022-10-21 20:11:01 -0600

    'yasehizocirculartocaresolvercolisionesdetablahash'

--- Hastable.py
+++ Hastable.py
@@ -4,6 +4,7 @@ import numpy as np
 class jacinto():
     def __init__(self):
         self.head = None
+        self.last = None
         self.tamaño = 13
         self.ocupacion = 0
         for n in range(self.tamaño):
@@ -12,11 +13,18 @@ class jacinto():
     def inicia_tamaño(self):
         if self.head == None:
             self.head = nodito('')
+            self.last = self.head
+            self.head.Next = self.last
+            self.last.Next = self.head
         else:
             k = self.head
-            while k.Next != None:
+            
+            while k.Next != self.head:
+                #print(f'acaentra{k}')
                 k = k.Next  
             k.Next = nodito('')
+            self.last = k.Next
+            self.last.Next = self.head
     def hasheo(self, cadena):
         general = sum(bytearray(cadena,encoding='utf-8'))
         print(f'NUMEROHASH_____{general}')
@@ -27,16 +35,22 @@ class jacinto():
         k = self.head
         for n in range(posicion):
             k = k.Next
-        k.value = value
+        if k.value != None:
+            k.value = value
+            self.ocupacion = self.ocupacion +1
+        else:
+            #aca va la resolucion de colision
+            pass
 
 
     
     def __str__(self) -> str:
         general = "TABLA HASH--------"
         k = self.head
-        while k!= None:
+        while k!= self.last:
             general+=f'\n{k}'
             k = k.Next
+        general+=f'\n{self.last}'
         return general
         
 pedro = jacinto()
