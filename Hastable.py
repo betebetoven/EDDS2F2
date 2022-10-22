4afac8bdfecaf51d5137783c134bb5deed88f36b
betebetoven <albertoha2000@gmail.com>
2022-10-21 23:30:59 -0600

    'faltaimplementarcolisionyacciondeporcentajedeocupacion'

--- Hastable.py
+++ Hastable.py
@@ -8,8 +8,13 @@ class jacinto():
         self.last = None
         self.tamaño = 13
         self.ocupacion = 0
+        self.porcentaje_de_ocupacion = 0
         for n in range(self.tamaño):
             self.inicia_tamaño()
+
+    def definir_porcentaje_ocupacion(self):
+        self.porcentaje_de_ocupacion = self.ocupacion/self.tamaño
+        return self.porcentaje_de_ocupacion    
 
     def inicia_tamaño(self):
         if self.head == None:
